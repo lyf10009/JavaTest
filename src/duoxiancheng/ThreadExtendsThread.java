5eadcaf025b37f3ec7b2f706e3538d8fd7f7394c
luo.fei <luofei>
2014-03-20 09:41:38 +0800

    多线程 join()方法的运用

--- src/duoxiancheng/ThreadExtendsThread.java
+++ src/duoxiancheng/ThreadExtendsThread.java
@@ -21,7 +21,16 @@ public class ThreadExtendsThread {
     public static void main(String[] args) {
 		MyMoreThread thread1=new MyMoreThread("第一个线程");
 		MyMoreThread thread2=new MyMoreThread("第二个线程");
+		MyMoreThread thread3=new MyMoreThread("第三个线程");
+		
 		thread1.start();
+		try {
+            thread1.join();//先执行完当前线程，在执行其他的
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+		thread3.start();
 		thread2.start();
+		
 	}
 }
